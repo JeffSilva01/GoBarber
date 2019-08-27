8f64c5c052ca143697f628737d68ff0bdcb238d2
jeffsilva01 <jeff_silva01@yahoo.com>
2019-08-27 14:13:05 -0300

    Projeto GoBarber desenvolvido nas aulas da Rocketseat

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
+import Dashboard from '~/pages/Dashboard';
+import Profiles from '~/pages/Profiles';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profiles" component={Profiles} isPrivate />
+
+      <Route path="*" component={() => <h1>404</h1>} isPrivate />
+    </Switch>
+  );
+}
